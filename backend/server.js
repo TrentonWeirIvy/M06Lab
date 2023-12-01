9611922a713d1807fe99371fd776785ee83538ef
Trenton Weir <81660030+TrentonWeir@users.noreply.github.com>
2023-12-01 16:29:47 -0500

    Updates

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,26 @@ const cors = require('cors');
 const port = 3000;
 
 app.use(cors());
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
-app.get('/messages',(req,res) => {
-    const messages = Array.from({length:10}, (_,index)=>{
-        return "Message" + index;
-    });
+var messages = Array.from({ length: 10 }, (_, index) => {
+    return "Message" + index;
+});
+
+app.get('/messages', (req, res) => {
     res.send(messages);
 });
 
 app.post('/messages', (req, res) => {
-    console.log(req.body);
-    res.json({});
+    let msg = req.body;
+
+    console.log(msg);
+    messages.push(msg.message);
+
+    res.json(messages);
 });
 
 app.listen(
     port,
-     () => console.log(`app running on http://localhost:${port}`)
+    () => console.log(`app running on http://localhost:${port}`)
 );
